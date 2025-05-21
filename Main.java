45b17f25992a55ba62ab68d0ed1f2d671743fe08
xeno <118031275+AnonymousXeno@users.noreply.github.com>
2025-05-21 17:49:21 -0400

    meaoo

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("name: ");
+        String a = s.next();
+        System.out.println("you are " + a);
+    }
+}
